5e13ad07013c661d39da4a14527f3e335ffd2265
Mike Parker <mgp@khanacademy.org>
2014-07-01 15:00:34 -0700

    Add formula for Homebrew for iOS development

---
+++ mogenerator.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Mogenerator < Formula
+  homepage "https://github.com/rentzsch/mogenerator"
+
+  head "https://github.com/rentzsch/mogenerator.git", :revision => "258832"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-target", "mogenerator", "-configuration", "Release","SYMROOT=symroot", "OBJROOT=objroot"
+    bin.install "symroot/Release/mogenerator"
+  end
+end
