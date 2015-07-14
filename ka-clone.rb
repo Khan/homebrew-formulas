8c65b0066c6f2ed8f4b78d64a0f41770186da374
Matthew Rothenberg <mroth@khanacademy.org>
2015-07-14 16:31:32 -0700

    Add ka-clone
    Since ka-clone now has versioned releases, make it installable via
    homebrew as well.
    
    Test Plan:
    brew audit ka-clone
    brew test ka-clone
    
    Auditors: ethan

---
+++ ka-clone.rb
@@ -0,0 +1,18 @@
+class KaClone < Formula
+  desc "Manages an isolated local gitconfig for cloned repositories"
+  homepage "https://github.com/khan/ka-clone"
+
+  url "https://github.com/Khan/ka-clone/archive/v1.0.2.tar.gz"
+  sha256 "153ce8c7900bba546ca31d2ceb7ef5a404e53e7cb414691015e2e7d96275e441"
+  head "https://github.com/khan/ka-clone.git"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    bin.install "bin/ka-clone"
+  end
+
+  test do
+    system "ka-clone --help"
+  end
+end
