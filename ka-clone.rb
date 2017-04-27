489890010cfba7507b7466f353f75bc215fc3268
Matthew Rothenberg <mroth@khanacademy.org>
2017-04-27 17:04:40 -0400

    bump kaclone to v1.1.0 tag
    Test plan: `brew uninstall ka-clone && brew install ka-clone`

--- ka-clone.rb
+++ ka-clone.rb
@@ -2,12 +2,8 @@ class KaClone < Formula
   desc "Manages an isolated local gitconfig for cloned repositories"
   homepage "https://github.com/khan/ka-clone"
 
-  url "https://github.com/Khan/ka-clone/archive/v1.0.4.tar.gz"
-  sha256 "d33e9753ad15ad31af920541a64590f5a560ebbb0847aabeef19c6d04dbc0f70"
-  # the revision should be omitted (default 0) unless you want to force a
-  # reinstall without a corresponding version bump, see: http://git.io/vm4VO
-  # revision 0
-
+  url "https://github.com/Khan/ka-clone/archive/v1.1.0.tar.gz"
+  sha256 "991195c7fe46ae5a278a0b0164b633a5a95b7ff7c69309abd637454008b2c7f8"
   head "https://github.com/khan/ka-clone.git"
 
   depends_on :python if MacOS.version <= :snow_leopard
